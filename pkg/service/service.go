06a40a0f0ac8b01d2143fe2e886a85a62b2b63fd
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2023-10-11 19:13:38 +0200

    Add Service Interface

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,3 +1,39 @@
 package service
 
-type Service struct{}
+import "github.com/hellofreshdevtests/HFtest-platform-engineering-thearyanahmed/pkg/schema"
+
+type Service struct {
+	configRepo ConfigRepository
+}
+
+type ConfigRepository interface {
+	Store() error
+	Find() ([]schema.ConfigMap, error)
+	Update() error
+	Delete() error
+}
+
+func NewService(configRepo ConfigRepository) *Service {
+	return &Service{
+		configRepo: configRepo,
+	}
+}
+
+func NewInMemoryRepository() *Service {
+	return &Service{}
+}
+
+func (s *Service) Store() error {
+	return nil
+}
+
+// @todo note use filters and limit
+func (s *Service) Find() ([]schema.ConfigMap, error) {
+	return s.configRepo.Find()
+}
+func (s *Service) Update() error {
+	return nil
+}
+func (s *Service) Delete() error {
+	return nil
+}
